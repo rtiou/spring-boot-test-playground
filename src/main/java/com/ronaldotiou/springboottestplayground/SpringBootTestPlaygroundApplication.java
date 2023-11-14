400738028cf05f46790209614322d612386ebf84
Ronaldo Tiou <rtiou@users.noreply.github.com>
2023-11-14 15:57:56 +0000

    Inital commit

---
+++ src/main/java/com/ronaldotiou/springboottestplayground/SpringBootTestPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.ronaldotiou.springboottestplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronaldotiou/springboottestplayground/SpringBootTestPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronaldotiou.springboottestplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
